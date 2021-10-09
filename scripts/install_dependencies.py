3a16807fb2a8ed28606f729dd4c0d565d219f1a8
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2021-10-09 10:08:41 +0900

    tmp

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -8,11 +8,12 @@ malt_dependencies_path = os.path.join(malt_folder, '.Dependencies-{}'.format(py_
 dependencies = ['numpy', 'glfw', 'PyOpenGL', 'PyOpenGL_accelerate', 'Pyrr', 'psutil']
 try:
     subprocess.check_call([sys.executable, '-m', 'venv', malt_dependencies_path])
+    subprocess.check_call(['ls', '-a', malt_folder])
     subprocess.check_call([os.path.join(malt_dependencies_path, 'Scripts/activate')])
     for dependency in dependencies:
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', dependency])
 except:
-    print('ERROR: pip install {} failed.'.format(dependency))
+    print('ERROR: pip install failed.')
     import traceback
     traceback.print_exc()
 
